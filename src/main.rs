3267b9110733ae0b50da3d6043a1162382695a5d
Grigory Latyshev <thegriglat@gmail.com>
2023-06-25 04:32:04 +0600

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use config::Config;
 use dotenv::dotenv;
 use state::AppState;
@@ -13,12 +13,15 @@ async fn main() -> std::io::Result<()> {
 
     let config = Config::read();
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     let app_data = web::Data::new(AppState::new());
     HttpServer::new(move || {
         App::new()
             .app_data(app_data.clone())
             .service(api::links::get_link)
             .service(api::links::post_link)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", config.port))?
     .workers(config.workers)
